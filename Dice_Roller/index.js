8e19981fe0c5f53253a74a65fc4d4efea6dedc80
Sanjay Kalagarla <sanjaykalagarla19@gmail.com>
2025-12-09 23:37:28 +0530

    Update index.js

--- Dice_Roller/index.js
+++ Dice_Roller/index.js
@@ -12,7 +12,7 @@ function rollDice(){
     for(let i=0;i<text.value;i++){
         const value=1+Math.floor(Math.random()*6);
         values.push(value);
-        images.push(`<img src=faces/${value}.jpg>`);
+        images.push(`<img src=images/${value}.jpg>`);
     }
 
     diceResult.textContent=`${values.join(', ')}`;
